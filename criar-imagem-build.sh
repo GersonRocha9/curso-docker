513be571115ea74cd51256cfd2e3e32ce8fbe3b1
Gerson Rocha <gersonrocha9@gmail.com>
2020-07-23 23:13:16 -0300

    Delete criar-imagem-build.sh

--- criar-imagem-build.sh
+++
@@ -1,6 +0,0 @@
-#dentro da pasta do build
-docker image build -t ex-simple-build .
-
-docker image ls #ver se foi criada
-
-docker container run -p 80:80 ex-simple-build #executando o container dentro da imagem criada
